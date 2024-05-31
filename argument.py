5248bdd03a1a8e91bd3b79b5ed39d20155f03149
anton572 <135094776+anton572@users.noreply.github.com>
2024-05-31 08:15:39 +0300

    TelegramBot_ostinFCT

--- argument.py
+++ argument.py
@@ -1,2 +1,5 @@
 import os
 token=os.environ.get('API_token_bot')
+Token_WEB=os.environ.get('Token_WEB')
+Start_WEB=os.environ.get('Start_WEB')
+Stop_WEB=os.environ.get('Stop_WEB')
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import conect.telegram as telegram
-import conect.FlaskServer as FlaskServer
 import on_off_server
 import argument
 import threading
@@ -9,27 +8,17 @@ def start(token):
     try:
         loger=Loger.Loger()
         telegrambot=telegram.telegrambot(token,Loger=loger.get_loger("TeleBot"))
-        beck=FlaskServer.FlaskServer(Loger=loger.get_loger("Beck"))
-        server=on_off_server.server('https://www.youtube.com/','https://','https://',Loger=loger.get_loger("ServControl"))
+        server=on_off_server.server(argument.Token_WEB,argument.Start_WEB,argument.Stop_WEB,Loger=loger.get_loger("ServControl"))
 
         loger.print("telegrambot init")
         telegrambot.init()
-        loger.print("beck init")
-        beck.init()
 
-        def iswork():
-            return {'work':telegrambot.iswork()}
         telegrambot.commands.set('start_factorio',server.strart)
-        beck.subscraibs(iswork,'/isworktelegrambot')
 
 
-        trase=threading.Thread(target=beck.run)
-        loger.print("beck start")
-        trase.start()
         loger.print("telegrambot start")
         telegrambot.run()
     finally:
-        del trase
         loger.close()
 
 if __name__ == '__main__':
